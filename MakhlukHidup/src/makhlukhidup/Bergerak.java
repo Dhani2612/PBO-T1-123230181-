0b7ab8b6d6b5a39ee0ca6b702d42d740dc83bac7
Dhani2612 <157573627+Dhani2612@users.noreply.github.com>
2025-02-25 18:21:23 +0700

    Create Bergerak.java

---
+++ MakhlukHidup/src/makhlukhidup/Bergerak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package makhlukhidup;
+
+/**
+ *
+ * @author Windows
+ */
+interface Bergerak {
+    void berjalan();
+    void berlari();
+    void melompat();
+}
+
